82791e3ca8566009621a9acaca9ac6f3e2cd7733
Yassin <yassiny78dz@gmail.com>
2025-08-15 00:55:09 +0100

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -1,1 +1,9 @@
+local bannedPlayers = {
+    [123456789] = true,
+    [123456789] = true,
+}
 
+local player = game.Players.LocalPlayer
+if bannedPlayers[player.UserId] then
+    player:Kick("You either cannot access this script, or are banned for a reason. (Staff, etc.)")
+end
